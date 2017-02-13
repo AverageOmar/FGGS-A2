dc5adc68302cb23bf6156ac4e8c932c6ab7ddefe
Omar Hassan <AverageOmar@users.noreply.github.com>
2017-02-13 10:20:09 +0000

    Create Transform.cpp

---
+++ Transform.cpp
@@ -0,0 +1,19 @@
+#include "Transform.h"
+
+
+Transform::Transform()
+{
+	_position = Vector();
+	_rotation = Vector();
+	_scale = Vector(1.0f, 1.0f, 1.0f);
+}
+
+Transform::~Transform()
+{
+
+}
+
+void Transform::Update()
+{
+	
+}
